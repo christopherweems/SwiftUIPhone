33744959e169f0828a1ee7c330901257ea3c92ac
Christopher Weems <hello@christopherweems.com>
2021-10-19 19:57:45 -0400

    Add `View.safeAreaInset(top:leading:bottom:trailing:)`

--- Sources/SwiftUIPhone/Extensions.swift
+++ Sources/SwiftUIPhone/Extensions.swift
@@ -26,3 +26,34 @@ extension Image {
         #endif
     }
 }
+
+extension View {
+    func safeAreaInset(top: CGFloat = 0, leading: CGFloat = 0, bottom: CGFloat = 0, trailing: CGFloat = 0) -> some View {
+        let insets = EdgeInsets(top: top, leading: leading, bottom: bottom, trailing: trailing)
+        return self.safeAreaInset(insets)
+    }
+    
+    @ViewBuilder func safeAreaInset(_ insets: EdgeInsets) -> some View {
+        if #available(macOS 12.0, iOS 15.0, *) {
+            self
+                .safeAreaInset(edge: .top, content: {
+                    Spacer().frame(height: insets.top)
+                })
+                .safeAreaInset(edge: .leading, content: {
+                    Spacer().frame(width: insets.leading)
+                })
+                .safeAreaInset(edge: .trailing, content: {
+                    Spacer().frame(width: insets.trailing)
+                })
+                .safeAreaInset(edge: .bottom, content: {
+                    Spacer().frame(height: insets.bottom)
+                })
+            
+        } else {
+            // does nothing on macOS 11 & below
+            self
+        }
+    }
+    
+}
+
